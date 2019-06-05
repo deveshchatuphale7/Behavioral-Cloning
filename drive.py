e9a8ec70523fa6b9eea7af627a03a13b2b555c30
deveshchatuphale7 <dcdevesh3@gmail.com>
2019-06-06 01:18:47 +0530

    with writeup

--- drive.py
+++ drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -61,7 +63,11 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        image2 = cv2.cvtColor(image_array, cv2.COLOR_BGR2HSV)
+        tmp = image2[:,:,1].reshape(160, 320, 1)
+
+        steering_angle = float(model.predict(tmp[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
@@ -136,4 +142,4 @@ if __name__ == '__main__':
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
